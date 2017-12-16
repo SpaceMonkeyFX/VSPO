ef4f33ccdf66d5024093518701464a8fa443ef91
SpaceMonkeyFX <34340846+SpaceMonkeyFX@users.noreply.github.com>
2017-12-16 20:13:08 +0100

    Add files via upload

---
+++ poslji_email.ps1
@@ -0,0 +1,25 @@
+﻿$Datum = $(get-date -f dd.MM.yyyy)
+
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+$Username = "monitoring@"
+$Password = "***geslo***"
+
+$to = "moj@gmail.com"
+#$cc = "user2@domain.com"
+$subject = "Log $Datum"
+$body = "Dodal sem priponko log."
+$attachment = "C:\$datum.log" 
+
+$message = New-Object System.Net.Mail.MailMessage
+$message.subject = $subject
+$message.body = $body
+$message.to.add($to)
+#$message.cc.add($cc)
+$message.from = $username
+$message.attachments.add($attachment)
+
+$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+$smtp.EnableSSL = $true
+$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$smtp.send($message)
---
+++ ustvari_log.ps1
@@ -0,0 +1,9 @@
+﻿$Ura = $(get-date -f HH:mm:ss)
+$Datum = $(get-date -f dd.MM.yyyy)
+$pot = "C:\$datum.log"
+Get-WmiObject -Class Win32_Product | Select-Object -Property Name | Sort-Object Name >> "$pot"
+Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User | Format-List >> "$pot"
+while(1) {
+    get-process | where-object {$_.mainwindowhandle -ne 0} | select-object name, mainwindowtitle | Format-Table @{Name='Datum';Expression={$Datum}}, @{Name='Ura';Expression={$Ura}}, @{Expression={$_.name};Label="Ime Programa"}, @{Expression={$_.mainwindowtitle};Label="Trenutno Aktivno Okno"} >> "$pot"
+    start-sleep -seconds 600
+    }
